dc69561251b00d994255dc6f906e1fc0a662ef7c
wannsim <S10242681@connect.np.edu.sg>
2023-02-07 23:16:35 +0800

    - format indiv dish page - correct save button in profile page to 'remove'

--- indivdish.js
+++ indivdish.js
@@ -3,3 +3,14 @@ $(document).ready(function(){
         $('ul').toggleClass('show')
     })
 })
+
+var r = document.querySelector('.rName')
+var rName = r.innerText;
+rName.replace(' ','')
+ r.classList.add(rName.replace(' ',''))
+
+
+ console.log(r)
+console.log(rName.replace(' ',''))
+var test = document.getElementsByClassName(rName.replace(' ',''))
+console.log(test)
